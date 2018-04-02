eb135f41d3ab1432db23551f6b391e8fb3859587
Luke Smith <lukedansmi@sbcglobal.net>
2018-04-01 23:55:00 -0500

    Needed for installing dependencies

---
+++ DESCRIPTION.R
@@ -0,0 +1,27 @@
+Package: HoustonCrimeViewer
+Title: Explore Houston crime data
+Version: 0.0.0.9000
+Date: 2018-03-28
+Authors@R: person(given = "Luke", 
+                  family = "Smith", 
+                  email = "lukedansmi@sbcglobal.net",
+                  role = c("aut", "cre"))
+Description: All the necessary files to replicate the shiny app.
+URL: https://github.com/seasmith/HoustonCrimeViewer/
+License: GPL (>= 3)
+LazyData: true
+Depends:
+  R (>= 3.4.3)
+Imports:
+  dplyr,
+  forcats,
+  ggplot2,
+  ggalt,
+  grid,
+  leaflet,
+  plotly,
+  scales,
+  sf,
+  shiny,
+  shinydashboard
+RoxygenNote: 6.0.1
