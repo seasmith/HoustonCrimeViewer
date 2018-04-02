9bb686ee70213d8bf0f3466f7f4563d19d502cab
Luke Smith <lukedansmi@sbcglobal.net>
2018-04-01 23:54:02 -0500

    Add plotly

--- server.R
+++ server.R
@@ -82,4 +82,149 @@ function(input, output, session) {
       )  %>%
       addLegend(position = "bottomleft", pal = colorPal(), values = ~rate, title = "Rate")
   })
+  
+  # LINES
+  output$yearly_trends <- renderPlotly({
+    
+    if (is.null(data$clickedShape)) {
+      
+      ggplotly(
+        select(as.data.frame(hpb_yearly_summed), -geometry) %>%
+          as_tibble() %>%
+          rename(Year = year, Rate = rate) %>%
+          mutate(Rate = round(Rate, 1)) %>%
+          ggplot() +
+          geom_line(aes(x = Year,
+                        y = Rate),
+                    color = "gray50", size = 0.8) +
+          facet_wrap(~`Offense Type`, scales = "free_y") +
+          scale_x_continuous(NULL, expand = expand_scale(c(0.1, 0.1)), breaks = pretty_breaks(3)) +
+          scale_y_continuous(expand = expand_scale(c(0.1, 0.1)), breaks = pretty_breaks(3)) +
+          labs(title = paste0("2010-2017 Crime in Houston"),
+               x = NULL,
+               y = paste0("Crime Rate (Per 100,000)")) +
+          theme_minimal(),
+        height = 400, width = 600
+      ) %>%
+        layout(xaxis = list(title = FALSE))
+      
+    } else {
+
+      ggplotly(
+        hpb_yearly %>%
+          filter(Beat == input$map_shape_click$id) %>%
+          as.data.frame() %>%
+          select(-geometry) %>%
+          dplyr::union(select(as.data.frame(hpb_yearly_summed), -geometry)) %>%
+          as_tibble() %>%
+          rename(Year = year, Rate = rate) %>%
+          mutate(Rate = round(Rate, 1)) %>%
+          ggplot() +
+          geom_line(aes(x = Year, y = Rate, color = Beat), size = 0.8) +
+          scale_x_continuous(NULL, expand = expand_scale(c(0.1, 0.1)), breaks = pretty_breaks(3)) +
+          scale_y_continuous(expand = expand_scale(c(0.1, 0.1)), breaks = pretty_breaks(3)) +
+          facet_wrap(~`Offense Type`, scales = "free_y") +
+          scale_color_manual(values = c("brown", "gray50")) +
+          guides(color = guide_legend(title = "")) +
+          labs(title = paste0("Crime in Houston 2010-2017"),
+               x = NULL,
+               y = paste0("Crime Rate (Per 100,000)")) +
+          theme_minimal(),
+        height = 400, width = 600
+      ) %>%
+        layout(xaxis = list(title = FALSE))
+    }
+    
+  })
+  
+  # DENSITY
+  output$density <- renderPlotly({
+    
+    output_density <- hpb_yearly %>%
+      filter(year == input$years) %>%
+      rename(Rate = rate) %>%
+      mutate(Rate = round(Rate, 1)) %>%
+      ggplot() +
+      geom_histogram(aes(Rate),
+                     alpha = 0.7, fill = "gray50") +
+      scale_x_continuous(expand = expand_scale(c(0.1,0.1))) +
+      scale_y_continuous(expand = expand_scale(c(0.1, 0.1)), breaks = pretty_breaks(2)) +
+      facet_wrap(~`Offense Type`, scales = "free") +
+      theme_minimal()
+    
+    if (is.null(data$clickedShape)) {
+      output_density <- output_density +
+        labs(title = paste0(input$years, " City of Houston Crime Distribution"),
+             x = paste0("Crime Rate (Per 100,000)"),
+             y = paste0("Number of Beats"))
+    } else {
+      output_density <- output_density +
+        geom_vline(aes(xintercept = Rate, text = paste0("Rate: ", round(Rate, 1))),
+                   data = filter(hpb_yearly, year == 2017, Beat == input$map_shape_click$id) %>%
+                     rename(Rate = rate) %>%
+                     mutate(Rate = round(Rate, 1)),
+                   color = "brown", size = 0.6, alpha = 0.7) +
+        labs(title = paste0(input$years, " Beat ", input$map_shape_click$id, " Crime Distribution"),
+             x = paste0("Crime Rate (Per 100,000)"),
+             y = paste0("Number of Beats"))
+    }
+    
+    ggplotly(output_density, height = 400, autosize = TRUE, width = 600)
+      
+  })
+  
+  # DAYS AND HOURS
+  output$day_and_hour_trends <- renderPlotly({
+    if (is.null(data$clickedShape)) {
+      # ggplot2:
+      ggplotly(
+        hpb_hourly_summed %>%
+          filter(`Offense Type` == input$offType &
+                   year == input$years) %>%
+          group_by(hour, week_day) %>%
+          summarize(n_offenses_sum  = sum(n_offenses, na.rm = TRUE),
+                    n_offenses_mean = mean(n_offenses, na.rm = TRUE)) %>%
+          ungroup() %>%
+          ggplot(aes(hour, fct_rev(week_day), fill = n_offenses_sum,
+                     text = paste0("Offenses: ", round(n_offenses_sum, 1), "\n",
+                                   "Day: ", fct_rev(week_day), "\n",
+                                   "Hour: ", x_labs[hour]))) +
+          geom_tile() +
+          scale_fill_viridis_c("Number of Offenses", option = "B") +
+          scale_x_continuous(NULL, expand = expand_scale(),
+                             breaks = seq(0, 20, 5), labels = x_labs[seq(1, 21, 5)]) +
+          scale_y_discrete(NULL, expand = expand_scale()) +
+          labs(title = paste0(input$years, " Day and Time Distribution of ", input$offType, " in Houston"),
+               x = NULL, y = NULL) +
+          theme_minimal(),
+        height = 400, autosize = TRUE, width = 600, tooltip = "text"
+      )
+    } else {
+      # ggplot2:
+      ggplotly(
+        hpb_hourly_summed %>%
+          filter(`Offense Type` == input$offType &
+                   Beat == input$map_shape_click$id &
+                   year == input$years) %>%
+          group_by(hour, week_day) %>%
+          summarize(n_offenses_sum  = sum(n_offenses, na.rm = TRUE),
+                    n_offenses_mean = mean(n_offenses, na.rm = TRUE)) %>%
+          ungroup() %>%
+          ggplot(aes(hour, fct_rev(week_day), fill = n_offenses_sum,
+                     text = paste0("Offenses: ", round(n_offenses_sum, 1), "\n",
+                                   "Day: ", fct_rev(week_day), "\n",
+                                   "Hour: ", x_labs[hour]))) +
+          geom_tile() +
+          scale_fill_viridis_c("Number of Offenses", option = "B") +
+          scale_x_continuous(NULL, expand = expand_scale(),
+                             breaks = seq(0, 20, 5), labels = x_labs[seq(1, 21, 5)]) +
+          scale_y_discrete(NULL, expand = expand_scale()) +
+          labs(title = paste0(input$years, " Day and Time Distribution of ", input$offType, " in Beat ", input$map_shape_click$id),
+               x = NULL, y = NULL) +
+          theme_minimal(),
+        height = 400, autosize = TRUE, width = 600
+      )
+    }
+  })
+  
 }
--- ui.R
+++ ui.R
@@ -10,6 +10,7 @@ header <- dashboardHeader(
 # SIDEBAR -----------------------------------------------------------------
 
 # Define 'Crime Category' inputs
+defaultCrimeType <- "Aggravated Assaults"
 crimeTypes <- c(
   "Aggravated Assaults" = "Aggravated Assaults",
   "Auto Thefts"         = "Auto Thefts",
@@ -21,94 +22,69 @@ crimeTypes <- c(
 )
 
 # Define 'Year' inputs
-yearValues <- as.character(2010:2017) %>% setNames(nm = .)
+defaultYearValue <- 2017
+yearValue <- as.character(2010:2017) %>% setNames(nm = .)
 
 # Set variables for use through out the ui
-mapText <- "Crime Map Explorer"
-databookText <- "Data Book"
-mapTabName   <- "map"
-databookTabName <- "databook"
+mapText <- "Crime Map"
+databriefText <- "Data Brief"
+mapTabName <- "map"
+databriefTabName <- "databrief"
 
 # Sidebar
 sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem(mapText, tabName = mapTabName),
-    menuItem(databookText, tabName = databookTabName),
-    selectInput("offType", "Crime Category", crimeTypes, "Aggravated Assaults"),
-    selectInput("years", "Year", yearValues, 2017)
+    menuItem(databriefText, tabName = databriefTabName),
+    selectInput("offType", "Crime Category", crimeTypes, defaultCrimeType),
+    selectInput("years", "Year", yearValue, defaultYearValue),
+    actionButton("resetBeatSelection", "Reset Beat Selection")
   )
 )
 
 
 # BODY --------------------------------------------------------------------
 
-
-# Set first row height to 500px...for now
+# Set row height
 mapRowHeight  <- "750px"
-# plotRowHeight <- "300px"  # not using this, for now
 
 # Body
 body <- dashboardBody(
-  
-  tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
+  tags$style(type = "text/css", "#map {height: calc(100vh - 100px) !important;}"),
   tabItems(
     tabItem(
-      tabName = mapTabName, h2(mapText),
+      tabName = mapTabName,
+      title = "Crime Map",
       fluidRow(
         box(
-          title = "Houston Crime Data Map", width = 8, height = mapRowHeight,
-          leafletOutput("map", width = "100%", height = "680px")
+          leafletOutput("map", width = "100%", height = "100%"), width = 7
         ),
         box(
-          title = "Police Beat Report", width = 4, height = mapRowHeight,
-          plotOutput("yearly_trends"),
-          plotOutput("day_and_hour_trends")
-        )#,
-        # box(
-        #   title = "Filters", width = 4, height = mapRowHeight,
-        #   selectInput("offType", "Crime Category", crimeTypes, "Aggravated Assaults"),
-        #   selectInput("years", "Year", yearValues, 2017)
-        # )
-      )#,
-      # fluidRow(
-      #   box(
-      #     title = "Police Beat Report",
-      #     plotOutput("yearly_trends"),
-      #     plotOutput("day_and_hour_trends")
-      #   )
-      # )
+          width = 5, height = "100%",
+          tabsetPanel(
+            type = "tabs",
+            tabPanel(
+              "Aggregate Plots",
+              plotlyOutput("yearly_trends", height = 400),
+              plotlyOutput("density", height = 400)
+              
+            ),
+            tabPanel(
+              "Individual Crime Plots",
+              plotlyOutput("day_and_hour_trends", height = 400)    
+            )
+          )
+        )
+      )
     ),
     tabItem(
-      tabName = databookTabName, h2(databookText)
+      tabName = databriefTabName, h2(databriefText)
     )
   )
 )
 
-# Experimental concept:
-# firstRow <- apply(tst, 1, function(x) set_box(x))  # cannot be used inside `fluidRow()`
-# fluidRow(apply(tst, 1, function(x) set_box(x)))
-# 
-# map_inputs    <- list(leafletOutput("map", width = "100%", height = "100%"))
-# select_inputs <- list(selectInput("offType", "Crime Category", crimeTypes, "Aggravated Assaults"),
-#                       selectInput("years", "Year", yearValues, 2017))
-# plot_outputs  <- list(plotOutput("yearly_trends"),
-#                       plotOutput("day_and_hour_trends"))
-# 
-# tst <- tibble::tribble(~title                  , ~width, ~height, ~input,
-#                        "Houston Crime Data Map",      8, "500px", map_inputs,
-#                        "Filters"               ,      4, "500px", select_inputs,
-#                        "Police Beat Report"    ,     12, "500px", plot_inputs)
-# 
-# set_box  <- function(r) box(title  = r[["title"]],  width = r[["width"]],
-#                             height = r[["height"]], eval(r[["input"]]))
-# 
-# tabItem(
-#   tabName = mapTabName, h2(mapText),
-#   fluidRow(apply(tst[1:2, ], 1, function(x) set_box(x))),
-#   fluidRow(apply(tst[3,   ], 1, function(x) set_box(x)))
-# )
 
 # UI ----------------------------------------------------------------------
 
 # ui
-ui <- dashboardPage(header, sidebar, body)
+ui <- dashboardPage(header, sidebar, body, skin = "purple")
